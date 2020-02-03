40e62e2fe86fa199e9f055629481533cc897b067
Boning Zhang <b0z00i6@m-c02w20xmhtdg.homeoffice.wal-mart.com>
2020-02-02 18:13:43 -0800

    update query

--- DAGS/sync_data_to_gcp/tasks/hqls/export_queries.py
+++ DAGS/sync_data_to_gcp/tasks/hqls/export_queries.py
@@ -3,7 +3,7 @@ ADD FILE {{ params.gcp_keyfile }};
 SET google.cloud.auth.service.account.enable=true;
 SET google.cloud.auth.service.account.json.keyfile={{ params.gcp_keyfile }};
 
-CREATE TABLE IF NOT EXISTS table1_{{ params.gen_date_str_nodash(execution_date, params.endDate_delta) }}_gcp_export(
+CREATE TABLE IF NOT EXISTS table1_{{ params.gen_date_str_nodash(execution_date) }}_gcp_export(
     column1 STRING,
     column2 DOUBLE
 ) PARTITIONED BY(
@@ -11,12 +11,12 @@ CREATE TABLE IF NOT EXISTS table1_{{ params.gen_date_str_nodash(execution_date, 
 )
 STORED AS PARQUET
 LOCATION
-  '{{ params.gcp_warehouse_path }}/gcp_database.db/table1_{{ params.gen_date_str_nodash(execution_date, params.endDate_delta) }}'
+  '{{ params.gcp_warehouse_path }}/gcp_database.db/table1_{{ params.gen_date_str_nodash(execution_date) }}'
 ;
 
 SET parquet.compression=GZIP;
 
-INSERT OVERWRITE TABLE table1_{{ params.gen_date_str_nodash(execution_date, params.endDate_delta) }}_gcp_export
+INSERT OVERWRITE TABLE table1_{{ params.gen_date_str_nodash(execution_date) }}_gcp_export
 PARTITION (ds = '{{ ds }}')
 SELECT column1,
        column2
@@ -39,7 +39,7 @@ LOCATION
   '{{ params.gcp_warehouse_path }}/gcp_database.db/table1'
 ;
 
-LOAD DATA INPATH '{{ params.gcp_warehouse_path }}/gcp_database.db/table1_{{ params.gen_date_str_nodash(execution_date, params.endDate_delta) }}/ds={{ ds }}/*'
+LOAD DATA INPATH '{{ params.gcp_warehouse_path }}/gcp_database.db/table1_{{ params.gen_date_str_nodash(execution_date) }}/ds={{ ds }}/*'
 OVERWRITE INTO TABLE table1 PARTITION (ds = '{{ ds }}')
 ;
 
@@ -50,5 +50,5 @@ ADD FILE {{ params.gcp_keyfile }};
 SET google.cloud.auth.service.account.enable=true;
 SET google.cloud.auth.service.account.json.keyfile={{ params.gcp_keyfile }};
 
-DROP TABLE IF EXISTS table1_{{ params.gen_date_str_nodash(execution_date, params.endDate_delta) }}_gcp_export;
+DROP TABLE IF EXISTS table1_{{ params.gen_date_str_nodash(execution_date) }}_gcp_export;
 """
